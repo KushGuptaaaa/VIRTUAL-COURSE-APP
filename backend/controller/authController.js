6b33a79215b6dded62d125575e7c6b24cd1a5865
KUSH KUMAR <158803292+KushGuptaaaa@users.noreply.github.com>
2026-06-13 12:23:10 +0530

    Update authController.js for deployment

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -38,8 +38,8 @@ export const signUp = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -74,8 +74,8 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -179,8 +179,8 @@ export const googleAuth = async (req, res) => {
         const token = await genToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "fal",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         return res.status(201).json(user);
